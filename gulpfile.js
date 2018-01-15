81a52f71b4ed2ac100a24b31e5858181d94bcdaf
grozamorei <desperatebassplayer@gmail.com>
2018-01-15 13:01:47 +0300

    ok, now gulpfile should actually work as intended..

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,15 @@ const gulp = require('gulp')
 gulp.task('clean', () =>
     gulp.src('build/', {read: false}).pipe(require('gulp-clean')())
 )
-gulp.task('build', ['clean'], () => {
-    require('nodemon')({
-        script: 'src/app.js',
-        env: { 'NODE_ENV': 'development' }
-    }).on('start', () => {
-        gulp.start('watch')
+gulp.task('build', ['clean'], () =>
+    require('child_process').exec('node ./src/app.js', (_, stdout, stderr) => {
+        console.log(stdout)
+        console.log(stderr)
     })
-})
+)
 
 gulp.task('watch', () => {
-    gulp.watch(['src/**/*.js'], ['clean'])
+    gulp.watch(['src/**/*.js', 'db/source.cdb', 'cs-templates/**/*.template'], ['build'])
 })
 
-gulp.task('default', ['build'])
+gulp.task('default', ['build', 'watch'])
